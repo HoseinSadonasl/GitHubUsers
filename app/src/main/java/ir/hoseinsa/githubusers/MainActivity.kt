ad0c301fd715b4f773ef96c65a26181996abae24
hoseincode <hoseinsadonasl@gmail.com>
2024-02-10 21:11:55 +0330

    improve ui

--- app/src/main/java/ir/hoseinsa/githubusers/MainActivity.kt
+++ app/src/main/java/ir/hoseinsa/githubusers/MainActivity.kt
@@ -1,26 +1,32 @@
 package ir.hoseinsa.githubusers
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import ir.hoseinsa.githubusers.ui.screens.users.UsersScreen
 import ir.hoseinsa.githubusers.ui.theme.GithubUsersTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,6 +34,27 @@ class MainActivity : ComponentActivity() {
                 val scope = rememberCoroutineScope()
                 val snackBarState = remember { SnackbarHostState() }
                 Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = stringResource(id = R.string.app_name)) },
+                            actions = {
+                                IconButton(
+                                    onClick = {
+                                        scope.launch {
+                                            snackBarState.showSnackbar(
+                                                message = "Hosein sadon asl"
+                                            )
+                                        }
+                                    },
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Outlined.Info,
+                                        contentDescription = "Information"
+                                    )
+                                }
+                            },
+                        )
+                    },
                     snackbarHost = { SnackbarHost(hostState = snackBarState) }
                 ) { innerPadding ->
                     Box(
