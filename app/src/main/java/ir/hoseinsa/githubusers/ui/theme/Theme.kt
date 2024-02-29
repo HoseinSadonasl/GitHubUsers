8d26133ac87410c01bf144baba8479ab4196691a
hoseincode <hoseinsadonasl@gmail.com>
2024-02-29 20:12:54 +0330

    fix: change statubar color

--- app/src/main/java/ir/hoseinsa/githubusers/ui/theme/Theme.kt
+++ app/src/main/java/ir/hoseinsa/githubusers/ui/theme/Theme.kt
@@ -58,8 +58,9 @@ fun GithubUsersTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            window.navigationBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
