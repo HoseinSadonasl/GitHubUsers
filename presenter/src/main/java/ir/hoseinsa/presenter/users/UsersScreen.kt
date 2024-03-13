a0345860c799baa52b2561543ece2c3241178fbd
hoseincode <hoseinsadonasl@gmail.com>
2024-03-14 03:05:54 +0330

    feat: improve loading state ui

--- presenter/src/main/java/ir/hoseinsa/presenter/users/UsersScreen.kt
+++ presenter/src/main/java/ir/hoseinsa/presenter/users/UsersScreen.kt
@@ -1,11 +1,15 @@
 package ir.hoseinsa.presenter.users
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.magnifier
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,10 +17,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import ir.hoseinsa.presenter.components.GitHubUsersTopAppBar
 import ir.hoseinsa.presenter.users.components.UserItemComponent
+import ir.hoseinsa.presenter.users.components.UserItemComponentLoading
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -44,35 +50,36 @@ fun UsersScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            when (refreshLoadState) {
-                is LoadState.Loading -> {
-                    CircularProgressIndicator()
-                }
-
-                is LoadState.Error -> {
-                    showSnackBar(refreshLoadState.error.message ?: "ERROR")
-                }
-
-                is LoadState.NotLoading -> {
-                    showSnackBar( "NotLoading")
-
-                }
-
-                else -> LazyColumn(
-                    state = rememberLazyListState()
-                ) {
-                    items(usersListState?.itemCount ?: 0) {
-                        val item = usersListState?.get(it)
-                        item?.let {
-                            UserItemComponent(
-                                userItem = item,
-                                onUserClick = { navigateToUserDetail(it) }
-                            )
-                        }?.run {
-                            Text(text = "No data!")
-                        }
+            LazyColumn {
+                showSnackBar("Loaded")
+                items(usersListState?.itemCount ?: 0) {
+                    val item = usersListState?.get(it)
+                    item?.let {
+                        UserItemComponent(
+                            userItem = item,
+                            onUserClick = { navigateToUserDetail(it) }
+                        )
                     }
                 }
+
+                when (refreshLoadState) {
+                    is LoadState.Loading -> {
+                        items(12) {
+                            UserItemComponentLoading()
+                        }
+                    }
+
+                    is LoadState.Error -> {
+                        showSnackBar("ERROR")
+                    }
+
+                    is LoadState.NotLoading -> {
+                        showSnackBar("NotLoading")
+                    }
+
+                    else -> {}
+                }
+
             }
         }
     }
--- presenter/src/main/java/ir/hoseinsa/presenter/users/components/UserItemComponent.kt
+++ presenter/src/main/java/ir/hoseinsa/presenter/users/components/UserItemComponent.kt
@@ -3,20 +3,25 @@ package ir.hoseinsa.presenter.users.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ir.hoseinsa.domain.users.model.user.UserItem
 import ir.hoseinsa.presenter.components.CircularImageComponent
+import kotlin.random.Random
 
 @Composable
 fun UserItemComponent(
@@ -37,14 +42,49 @@ fun UserItemComponent(
             imageUrl = userItem.avatar_url
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Row(modifier = Modifier.fillMaxWidth(),
+        Row(
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
-            ) {
+        ) {
             Column {
                 Text(text = userItem.login, style = MaterialTheme.typography.bodyLarge)
             }
         }
 
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserItemComponentLoading(
+) {
+    val randomWidth = Random.nextInt(from = 40, until = 160)
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+    ) {
+        Card(
+            modifier = Modifier
+                .height(48.dp)
+                .width(48.dp),
+            shape = RoundedCornerShape(percent = 50)
+        ) {
+
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Card(
+                modifier = Modifier
+                    .height(12.dp)
+                    .width(randomWidth.dp)
+                    .padding(end = 8.dp),
+                shape = RoundedCornerShape(percent = 50)
+            ) {}
+        }
     }
 }
